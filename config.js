5a2e396861f0c98df55068614104cf64f0b93c78
Shatovitsky <92272028+Shatovitsky@users.noreply.github.com>
2024-01-26 18:33:54 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    token: '6968029424:AAHeHE6lf15rAaxysno5iljvC5fyPVrBvm4',
-    webAppUrl: 'https://vulkan-vegas-hack.site/',
-    webAppUrl2: 'https://hack-vulkan-vegas.site/',
-    file_id: 'https://i.ibb.co/1d55gKv/Group-120.png',
-    file_id2: 'https://poltronanerd.com.br/wp-content/uploads/2023/04/lucky.webp'
-  };
-
-  module.exports = config;
